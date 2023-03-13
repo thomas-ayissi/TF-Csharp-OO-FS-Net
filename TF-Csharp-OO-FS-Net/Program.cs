13816da101340e58e0b9e77713dde8b173c4ee49
Sebastien <byasebastien@hotmail.com>
2023-03-13 09:34:40 +0100

    com

--- TF-Csharp-OO-FS-Net/Program.cs
+++ TF-Csharp-OO-FS-Net/Program.cs
@@ -1,32 +1,37 @@
 ﻿using TF_Csharp_OO_FS_Net.Veterinaire;
 
-Animal animal1 = new Animal("Poppy", "Levrier", "Blanc");
-Animal animal2 = new Animal("puppy", "poulet", "Noir");
-Animal animal3 = new Animal("poupy", "chat", "Tigré"); 
-Animal animal4 = new Animal("popol", "lapin", "Roux");
-Animal animal5 = new Animal("polette", "Taureau", "Brun");
-Animal animal6 = new Animal("jacky", "Baleine", "Bleu");
+//#region Animalerie
 
-Cabinet cabinet = new Cabinet("Michou le Cabinet");
+//Animal animal1 = new Animal("Poppy", "Levrier", "Blanc");
+//Animal animal2 = new Animal("puppy", "poulet", "Noir");
+//Animal animal3 = new Animal("poupy", "chat", "Tigré"); 
+//Animal animal4 = new Animal("popol", "lapin", "Roux");
+//Animal animal5 = new Animal("polette", "Taureau", "Brun");
+//Animal animal6 = new Animal("jacky", "Baleine", "Bleu");
 
-cabinet.Ajouter(animal1);
+//Cabinet cabinet = new Cabinet("Michou le Cabinet");
 
-cabinet.Ajouter(animal2);
+//cabinet.Ajouter(animal1);
 
-cabinet.Ajouter(animal3);
+//cabinet.Ajouter(animal2);
 
-cabinet.Ajouter(animal4);
+//cabinet.Ajouter(animal3);
 
-cabinet.Ajouter(animal5);
+//cabinet.Ajouter(animal4);
 
-cabinet.Ajouter(animal6);
+//cabinet.Ajouter(animal5);
 
-cabinet.Lister();
+//cabinet.Ajouter(animal6);
 
-cabinet.Supprimer(animal4);
+//cabinet.Lister();
 
-cabinet.Lister();
+//cabinet.Supprimer(animal4);
 
-cabinet.Ajouter(animal2);
+//cabinet.Lister();
 
-cabinet.Supprimer(animal4);
+//cabinet.Ajouter(animal2);
+
+//cabinet.Supprimer(animal4);
+
+//#endregion
+
